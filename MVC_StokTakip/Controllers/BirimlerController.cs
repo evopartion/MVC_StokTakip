fdb86c44007099cf6eb32eacef833b908eaa6258
evopartion <61056779+evopartion@users.noreply.github.com>
2025-02-09 02:36:05 +0300

    Kategori ve Birim Listeleme

---
+++ MVC_StokTakip/Controllers/BirimlerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_StokTakip.Controllers;
+
+using MVC_StokTakip.Models.Entity;
+
+namespace MVC_StokTakip.Controllers
+{
+    public class BirimlerController : Controller
+    {
+        // GET: Birimler
+        MVC_StokTakipEntities db=new MVC_StokTakipEntities();   
+        public ActionResult Index()
+        {
+            return View(db.Birimler.ToList());
+        }
+    }
+}
---
+++ MVC_StokTakip/Controllers/KategorilerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_StokTakip.Models.Entity;
+
+namespace MVC_StokTakip.Controllers
+{
+    public class KategorilerController : Controller
+    {
+        // GET: Kategoriler
+        MVC_StokTakipEntities db=new MVC_StokTakipEntities();
+        public ActionResult Index()
+        {
+            return View(db.Kategoriler.ToList());
+        }
+    }
+}
