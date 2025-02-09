aca7b2e0eb4b825bc5a9a22b89132efcd7e3a1f0
evopartion <61056779+evopartion@users.noreply.github.com>
2025-02-10 02:01:03 +0300

    Login Sayfası Oluşturma

---
+++ MVC_StokTakip/Controllers/KullanicilarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_StokTakip.Controllers
+{
+    public class KullanicilarController : Controller
+    {
+        // GET: Kullanicilar
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
