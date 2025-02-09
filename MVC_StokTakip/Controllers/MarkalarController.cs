e58f0c1be9f18c86725cb0428b9db533c20554f3
evopartion <61056779+evopartion@users.noreply.github.com>
2025-02-09 04:45:03 +0300

    Marka Listeleme - DropdownList

---
+++ MVC_StokTakip/Controllers/MarkalarController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using MVC_StokTakip.Models.Entity;
+
+namespace MVC_StokTakip.Controllers
+{
+    public class MarkalarController : Controller
+    {
+        // GET: Markalar
+        MVC_StokTakipEntities db=new MVC_StokTakipEntities();
+        public ActionResult Index()
+        {
+            var model=db.Markalar.ToList();
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            var model=new Markalar();
+            ViewBag.KategoriID = new SelectList(db.Kategoriler, "ID", "Kategori", model.KategoriID);
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Ekle(Markalar m)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
