2e393f034ae09423823a0c1e775eb41d19bd64fa
evopartion <61056779+evopartion@users.noreply.github.com>
2025-02-12 18:54:14 +0300

    Sepet Tutarını Hesaplama

--- MVC_StokTakip/Controllers/SepetController.cs
+++ MVC_StokTakip/Controllers/SepetController.cs
@@ -18,6 +18,21 @@ namespace MVC_StokTakip.Controllers
                 var kullaniciAdi = User.Identity.Name;
                 var kullanici = db.Kullanicilar.FirstOrDefault(x=>x.KullaniciAdi==kullaniciAdi);
                 var model=db.Sepet.Where(x=>x.KullaniciID==kullanici.ID).ToList();
+                var kid = db.Sepet.FirstOrDefault(x=>x.KullaniciID==kullanici.ID);
+                if (model!=null)
+                {
+                    if (kid==null)
+                    {
+                        ViewBag.Tutar = "Sepetinizde ürün bulunmuyor";
+                    }
+                    else if (kid!=null)
+                    {
+                        Tutar = db.Sepet.Where(x => x.KullaniciID == kid.KullaniciID).Sum(x=>x.ToplamFiyati);
+                        ViewBag.Tutar = "Toplam Tutar=" + Tutar + "TL";
+
+                    }
+                    return View(model);
+                }
                 return View(model);
             }
             return HttpNotFound();
