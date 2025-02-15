4733b93d217c8d6eec3241b55e8c5e1535baab8b
evopartion <61056779+evopartion@users.noreply.github.com>
2025-02-15 20:22:04 +0300

    Modal-Popup Listeleme

---
+++ MVC_StokTakip/Controllers/Kategoriler2Controller.cs
@@ -0,0 +1,53 @@
+﻿using MVC_StokTakip.Models.Entity;
+using MVC_StokTakip.MyModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_StokTakip.Controllers
+{
+    public class Kategoriler2Controller : Controller
+    {
+        MVC_StokTakipEntities db=new MVC_StokTakipEntities();
+        // GET: Kategoriler2
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetKategoriList()
+        {
+            List<MyKategoriler>liste=db.Kategoriler.Select(x=>new MyKategoriler
+            {
+                ID = x.ID,
+                Kategori = x.Kategori,
+                Aciklama = x.Aciklama
+            }).ToList();
+            return Json(liste,JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost,ValidateAntiForgeryToken]
+        public ActionResult Ekle(Kategoriler p)
+        {
+            db.Entry(p).State=System.Data.Entity.EntityState.Added;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult Guncelle(Kategoriler p)
+        {
+            db.Entry(p).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult Sil(Kategoriler p)
+        {
+            db.Entry(p).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
