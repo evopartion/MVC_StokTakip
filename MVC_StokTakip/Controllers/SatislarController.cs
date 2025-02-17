e889496a4fa65d4967c0f870effcbe7399f21b74
evopartion <61056779+evopartion@users.noreply.github.com>
2025-02-18 01:04:17 +0300

    Satın Alma İşlemi

--- MVC_StokTakip/Controllers/SatislarController.cs
+++ MVC_StokTakip/Controllers/SatislarController.cs
@@ -10,11 +10,50 @@ namespace MVC_StokTakip.Controllers
     public class SatislarController : Controller
     {
         // GET: Satislar
-        MVC_StokTakipEntities db=new MVC_StokTakipEntities();
+        MVC_StokTakipEntities db = new MVC_StokTakipEntities();
         public ActionResult Index()
         {
-            var model= db.Satislar.ToList();
+            var model = db.Satislar.ToList();
             return View(model);
+        }
+        public ActionResult SatinAl(int id)
+        {
+            var model = db.Sepet.FirstOrDefault(x => x.ID == id);
+            return View(model);
+        }
+        public ActionResult SatinAl2(int id)
+        {
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var model=db.Sepet.FirstOrDefault(x => x.ID==id);
+                    var satis = new Satislar
+                    {
+                        KullaniciID = model.KullaniciID,
+                        UrunID = model.UrunID,
+                        SepetID = model.ID,
+                        BarkodNo = model.Urunler.BarkodNo,
+                        BirimFiyati = model.BirimFiyati,
+                        Miktari = model.Miktari,
+                        KDV = model.Urunler.KDV,
+                        BirimID = model.Urunler.BirimID,
+                        Tarih = DateTime.Now,
+                        Saat = DateTime.Now
+                    };
+                    db.Sepet.Remove(model);
+                    db.Satislar.Add(satis);
+                    db.SaveChanges();
+                    ViewBag.islem = "Satın alma başarılı";
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.islem = "Satın alma başarısız";
+            }
+           
+            return View("islem");
         }
     }
 }
