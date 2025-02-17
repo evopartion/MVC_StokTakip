788b0d1f6628324ba462c057579f3394cd980a5e
evopartion <61056779+evopartion@users.noreply.github.com>
2025-02-18 01:36:21 +0300

    Birden Fazla Ürün Satışı

--- MVC_StokTakip/Controllers/SatislarController.cs
+++ MVC_StokTakip/Controllers/SatislarController.cs
@@ -57,5 +57,32 @@ namespace MVC_StokTakip.Controllers
            
             return View("islem");
         }
+
+        public ActionResult HepsiniSatinAl(decimal? Tutar)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var kullaniciAdi = User.Identity.Name;
+                var kullanici = db.Kullanicilar.FirstOrDefault(x => x.KullaniciAdi == kullaniciAdi);
+                var model = db.Sepet.Where(x => x.KullaniciID == kullanici.ID).ToList();
+                var kid = db.Sepet.FirstOrDefault(x => x.KullaniciID == kullanici.ID);
+                if (model != null)
+                {
+                    if (kid == null)
+                    {
+                        ViewBag.Tutar = "Sepetinizde ürün bulunmuyor";
+                    }
+                    else if (kid != null)
+                    {
+                        Tutar = db.Sepet.Where(x => x.KullaniciID == kid.KullaniciID).Sum(x => x.ToplamFiyati);
+                        ViewBag.Tutar = "Toplam Tutar=" + Tutar + "TL";
+
+                    }
+                    return View(model);
+                }
+                return View(model);
+            }
+            return HttpNotFound();
+        }
     }
 }
